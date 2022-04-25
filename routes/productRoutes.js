b7d32332410921f5fdb112aca52530598fe1435e
engam <engamrdiab52@gmail.com>
2022-04-25 10:15:14 +0200

    initial

---
+++ routes/productRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  createProduct,
+  getAllProducts,
+} = require('../controllers/productController');
+const { uploadProductImage, uploadProductImageLocal } = require('../controllers/uploadsController');
+
+router.route('/').post(createProduct).get(getAllProducts);
+router.route('/uploads').post(uploadProductImage);
+router.route('/uploadlocal').post(uploadProductImageLocal)
+
+module.exports = router;
